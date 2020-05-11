6e31e7dae83c29cbac1065e3fa9fc4e151c95fc8
alex <geniofuturo@gmail.com>
2020-05-11 16:10:06 -0300

    Fix redirect loop.

--- django_chatter/urls.py
+++ django_chatter/urls.py
@@ -10,11 +10,11 @@ except ImportError:
 app_name = 'django_chatter'
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name="index"),
-    path('chat/(?P<uuid>.+)/', views.ChatRoomView.as_view(), name="chatroom"),
+    path('^$', views.IndexView.as_view(), name="index"),
+    path('chat/(?P<uuid>.+)/$', views.ChatRoomView.as_view(), name="chatroom"),
 
     # AJAX paths
     path('ajax/users-list/', views.users_list, name="users_list"),
     path('ajax/get-chat-url/', views.get_chat_url, name="get_chat_url"),
-    path('ajax/get-messages/(?P<uuid>.+)/', views.get_messages, name="get_messages")
+    path('ajax/get-messages/(?P<uuid>.+)/$', views.get_messages, name="get_messages")
 ]
